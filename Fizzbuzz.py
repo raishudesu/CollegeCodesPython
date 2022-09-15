a39187f7cac90ec98a777a19c04d17c1e3016d29
raishudesu <105921989+raishudesu@users.noreply.github.com>
2022-09-15 21:33:51 +0800

    Update Fizzbuzz.py

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,15 +1,13 @@
 user = int(input())
-
 def fizzbuzz(user):
     for i in range(user):
         i += 1
-        if i%3 and i%5 == 0:
+        if i%3 == 0 and i%5 == 0: #to know if the value is divisible by 3 AND 5
             print('FIZZBUZZ')
-        elif i%3 == 0:
+        elif i%3 == 0:            #to know if the value is divisible by 3
             print('Fizz')
-        elif i%5 == 0:
+        elif i%5 == 0:            #to know if the value is divisible by 5
             print('Buzz')
         else:
             print(i)
-
 fizzbuzz(user)
